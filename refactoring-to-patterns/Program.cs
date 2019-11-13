a012099e5327f12a8d7877a8d224d9797b7357e4
andrei.dana <andrei.dana@192-168-0-107.rdsnet.ro>
2019-11-13 22:12:13 +0200

    initial commit

---
+++ refactoring-to-patterns/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace refactoring_to_patterns
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
